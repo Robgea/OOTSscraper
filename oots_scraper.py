8f3a2b7139d1532ff76d25aca6124bc96b3eb81d
Rob <Zhouenkissinger@gmail.com>
2019-06-08 15:24:27 -0400

    Now gets everything.

--- oots_scraper.py
+++ oots_scraper.py
@@ -6,31 +6,40 @@ import sys
 
 def comic_start():
     os.makedirs('oots', exist_ok = True)
-    urlnum = 1
-    totalnum = 1166
+    pagenum = 1
 
-    while urlnum < totalnum:
-        url = f'http://www.giantitp.com/comics/oots{str(urlnum).zfill(4)}.html'
+
+    baseurl = 'http://www.giantitp.com/'
+    url = 'http://www.giantitp.com/comics/oots0001.html'
+    while not url.endswith('#'):
         oots_comic = requests.get(url)
         oots_soup = BeautifulSoup(oots_comic.content, "html5lib")
         imgs = oots_soup.find_all('img',{'src':re.compile('/comics/images/.*')})
         if imgs == []:
-            print(f"Couldn't find comic number {urlnum}")
+            sys.stdout.write(f"Couldn't find comic number {pagenum}")
+            sys.stdout.flush()
         else:
             comimg = imgs[0].get('src')
-            comurl = f'http://www.giantitp.com/{comimg}'
+            comurl = f'{baseurl}{comimg}'
             comdown = requests.get(comurl)
             comend = comurl[-4:]
-            comname = f'oots{str(urlnum).zfill(4)}{comend}'
+            comname = f'oots{str(pagenum).zfill(4)}{comend}'
             sys.stdout.write('\n')
-            sys.stdout.write(f'Downloading comic number: {urlnum}')
+            sys.stdout.write(f'Downloading comic number: {pagenum}')
             sys.stdout.flush()
             imageFile = open(os.path.join('oots', os.path.basename(comname)), 'wb')
             for chunk in comdown.iter_content(100_000):
                 imageFile.write(chunk)
             imageFile.close()
 
-        urlnum += 1
+        next_image =oots_soup.find(title = 'Next Comic')
+        next_link = next_image.parent.get('href')
+        url = f'{baseurl}{next_link}'
+        pagenum += 1
+
+    sys.stdout.write('\n All done!')
+    sys.stdout.flush()
+
 
 def main():
     comic_start()
